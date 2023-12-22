0739d415833854e71e7560bee1f230f0b49008bf
cutyqueen <010829mii@gmail.com>
2023-12-22 16:36:05 +0900

    zz

--- Cafein/src/main/java/com/cafein/controller/SalesController.java
+++ Cafein/src/main/java/com/cafein/controller/SalesController.java
@@ -8,9 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.cafein.domain.SalesVO;
 import com.cafein.service.SalesService;
 
 @Controller
